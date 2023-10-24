cd86866e4ae4d01d5e5e1f84010215a5b306041a
eric <ericmitchell1625@hotmail.com>
2023-10-24 16:31:19 -0400

    commit

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,7 +1,41 @@
 sequence = [4, 3, 5, 0, 1]
-swaps = 0
 
-# Your Code Here
 
-print(f"Final result: {result}")
-print(f"Swaps: {swaps}")
+def bubble_sort(sequence):
+    swaps = 0
+    for i in range(len(sequence)):
+        for j in range(len(sequence)-1):
+            if sequence[j] > sequence[j+1]:
+                current = sequence[j]
+                sequence[j] = sequence[j+1]
+                sequence[j+1] = current
+            print(sequence)
+            swaps+=1
+    result = sequence
+    print(f"Final result: {result}")
+    print(f"Swaps: {swaps}")
+
+
+
+bubble_sort(sequence)
+
+
+
+
+# def bubble_sort(sequence):
+#     swaps = 0
+#     for i in range(len(sequence)):
+#         for j in range(len(sequence)-1):
+#             print(sequence)
+#             print(f'Comparing {sequence[i]} to {sequence[j]}')
+#             if sequence[i] < sequence[j]:
+#                 temp = sequence[i]
+#                 sequence[i] = sequence[j]
+#                 sequence[j] = temp
+#                 swaps+=1
+#     result = sequence
+#     print(f"Final result: {result}")
+#     print(f"Swaps: {swaps}")
+
+
+# bubble_sort(sequence)
