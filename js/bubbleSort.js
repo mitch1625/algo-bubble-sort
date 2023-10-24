ea659cc45927377fc34834607f5a24c334571d0e
eric <ericmitchell1625@hotmail.com>
2023-10-24 16:40:09 -0400

    JS

--- js/bubbleSort.js
+++ js/bubbleSort.js
@@ -1,7 +1,24 @@
 var sequence = [4, 3, 5, 0, 1]
 var swaps = 0
 
-// Your Code Here
+function bubbleSort(arr){
+    let swapCount = 0
+    for (let i = 0; i < arr.length;i++) {
+        for (let j = 0; j < arr.length-1; j++){
+            if (arr[j] > arr[j+1]){
+                let current = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = current
+                console.log(arr)
+            }
+            swapCount+=1;
+        }
+    }
+    // let result = arr
+    console.log(arr)
+}
 
-console.log("Final result: " + result)
-console.log("Swaps: " + swaps)
+
+bubbleSort(sequence)
+// console.log("Final result: " + result)
+// console.log("Swaps: " + swaps)
